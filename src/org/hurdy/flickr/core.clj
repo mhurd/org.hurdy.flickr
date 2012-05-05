de93411824e8bf117f9c2682702fa08f92658743
Mike Hurd <leahcim77@gmail.com>
2012-05-05 17:16:51 +0100

    NOTICKET: added threading for getting all the public photos

--- src/org/hurdy/flickr/core.clj
+++ src/org/hurdy/flickr/core.clj
@@ -5,6 +5,8 @@
 (use '[clojure.string :only (join)])
 (use '[clojure.xml :only (parse)])
 
+(import '(java.util.concurrent Executors))
+
 (def flickr-key "1c46840d769cbf7e2281680ea58a45ed")
 
 (def flickr-consumer-secret "2cd273980ccde5db")
@@ -13,58 +15,127 @@
 
 (def flickr-rest-api "http://api.flickr.com/services/rest/?")
 
-(defn create-param-string [keyvalues]
-  (for [[k v] keyvalues] (str k "=" v))
+(defn create-param-strings [& params]
+  ; accept a variable number of parameters in assumed key/value/key/value order
+  ; turn the list of parameters into a map, deconstruct each key/value pair one at a time
+  ; in a for loop then turn the pair into an '=' separated String and collect into
+  ; a list (via the for) and return the list
+  (for [[k v] (apply hash-map params)]
+    (str k "=" v))
   )
 
-(defn complete-request-params [method request-params-map]
-  (sort (concat (create-param-string {"method" method "format" "rest" "api_key" flickr-key "user_id" flickr-user-id}) (create-param-string request-params-map)))
+(defn complete-request-params [method-name & request-params]
+  ; accept a method name and a variable number of parameters.
+  ; create the basic set of parameters then create the extra
+  ; parameters, turn the results into one list and return the
+  ; list sorted (by key effectively)
+  (sort
+    (concat
+      (create-param-strings "method" method-name,
+        "format" "rest",
+        "api_key" flickr-key,
+        "user_id" flickr-user-id)
+      (apply create-param-strings request-params)))
   )
 
-(defn create-url [method request-params]
-  (str flickr-rest-api (clojure.string/join "&" (complete-request-params method request-params)))
+(defn create-url [method & request-params]
+  ; create the url to call by joining the base flickr rest
+  ; url with the processed parameters
+  (str
+    flickr-rest-api
+    (join "&" (apply complete-request-params method request-params)))
   )
 
-(defn flickr-people-getPublicPhotos [page numberOfPhotosPerPage]
-  (parse (create-url "flickr.people.getPublicPhotos" {"per_page" numberOfPhotosPerPage "page" page}))
+(defn match-tag [element expected-tag]
+  ; if the :tag of the specified element matches
+  ; the expectedTag the return the :attrs of the element
+  ; otherwise return nil
+  (if (= expected-tag (:tag element)) (:attrs element))
   )
 
-(defn match-tag [element expectedTag]
-  (if (= expectedTag (:tag element)) (:attrs element))
+(defn flickr-people-get-public-photos [page number-of-photos-per-page]
+  ; use the xml parse function to call the flickr.people.getPublicPhotos method
+  ; getting the specified page given the number of photos per page required
+  (parse
+    (create-url "flickr.people.getPublicPhotos" "per_page" number-of-photos-per-page "page" page))
   )
 
-(defn constructPhotoSourceURL [photo-attrs size]
-  (str "http://farm" (:farm photo-attrs) ".staticflickr.com/" (:server photo-attrs) "/" (:id photo-attrs) "_" (:secret photo-attrs) "_" size ".jpg")
-  )
-
-(defn getPublicPhotoSourceURLs
-  "Accepted sizes for photos are of the set [mstzb], medium, small, thumbnail, larger and even larger"
-  ([numberOfPhotosPerPage size]
-    (getPublicPhotoSourceURLs numberOfPhotosPerPage 1 size))
-  ([numberOfPhotosPerPage page size]
-    (println (str "Getting page " page))
-    (for [x
-        (map #(constructPhotoSourceURL % size)
-          (filter identity (map #(match-tag % :photo) (xml-seq (flickr-people-getPublicPhotos page numberOfPhotosPerPage)))))] x))
-  )
-
-(defn getPages [photosAttrs]
-  (Math/ceil (/ (Integer/parseInt (:pages photosAttrs)) 500))
-  )
-
-(defn getAllPublicPhotoSourceURLs
-  "Accepted sizes for photos are of the set [mstzb], medium, small, thumbnail, larger and even larger"
-  [size]
-    ; Get a sample page to determine the number of pages given a max size of 500 photos per-page
-    (let [pages (map #(getPages %)
-      (filter identity (map #(match-tag % :photos) (xml-seq (flickr-people-getPublicPhotos 1 1)))))]
-      ; count through and make each of the calls
-      (for [x (range 1 (+ 1 (first pages)) 1)]
-        (getPublicPhotoSourceURLs 500 x size))
+(defn construct-photo-source-url [photo-attrs size]
+  ; creates the actual photo URL given the photo element attributes as
+  ; retrieved from the Flickr API.
+  ; use let to extract the named keys from the supplied photo attributes map
+  ; and use them as local variables
+  (let [{:keys [farm server id secret]} photo-attrs]
+    (str
+      "http://farm"
+      farm
+      ".staticflickr.com/"
+      server
+      "/"
+      id
+      "_"
+      secret
+      "_"
+      size
+      ".jpg")
     )
   )
 
-(defn printAllSquarePublicPhotoSourceURLs []
-    (for [x (filter identity (flatten (getAllPublicPhotoSourceURLs "s")))]
-      (println x))
+(defn get-page-count [total-pages photos-per-page]
+  (Math/ceil (/ total-pages photos-per-page))
+  )
+
+(defn get-matching-tags [tag flickr-xml-seq]
+  (filter identity (map #(match-tag % tag) flickr-xml-seq))
+  )
+
+(defn get-public-photo-source-urls
+  ([number-of-photos-per-page size]
+    (get-public-photo-source-urls 1 number-of-photos-per-page size))
+  ([page number-of-photos-per-page size]
+    (println (str "Getting " number-of-photos-per-page " photos from page " page))
+    (for [x
+      (map #(construct-photo-source-url % size)
+        (filter identity
+          (map #(match-tag % :photo )
+            (xml-seq (flickr-people-get-public-photos page number-of-photos-per-page)))))]
+      x))
+  )
+
+(defn spawn-agents [agents range-of-pages]
+  (if (empty? range-of-pages)
+     agents
+     (recur (conj agents (agent (first range-of-pages)))
+        (rest range-of-pages))))
+
+(defn get-all-public-photo-source-urls [size]
+  ; Get a sample page to determine the number of pages given a max size of 500 photos per-page
+  (let
+    [max-photos-per-page 500
+    flickr-xml-seq (xml-seq (flickr-people-get-public-photos 1 1))
+    total-pages (:pages (first (get-matching-tags :photos flickr-xml-seq)))
+    pages (get-page-count (Integer/parseInt total-pages) max-photos-per-page)
+    page-range (range 1 (+ 1 pages) 1)
+    agents (spawn-agents [] page-range)
+    urls (ref [])]
+      ; send each agent a job to do
+      (println (str "Agent count = " (count agents)))
+      (doseq [agent agents]
+        (send agent #(get-public-photo-source-urls % max-photos-per-page size)))
+      ; ensure successful dispatch
+      (apply await agents)
+      ; view the results
+      (doseq [agent agents]
+        #_(println (str "Agent" agent " got " (count @agent) " photos"))
+        (dosync
+          (ref-set urls (flatten (conj @urls @agent))))
+        )
+      ; clean up although not right now as this is running in the repl
+      #_(shutdown-agents)
+      @urls
+    )
+  )
+
+(defn count-all-public-photo-source-urls []
+  (println (str "Got " (count (get-all-public-photo-source-urls "s")) " photos"))
   )
--- test/org/hurdy/flickr/test/core.clj
+++ test/org/hurdy/flickr/test/core.clj
@@ -4,19 +4,47 @@
 
 (require '[clj-http.client :as client])
 
-(deftest test-create-param-string
-  (is (= (create-param-string {"foo" "bar"}) '("foo=bar")))
+(deftest test-create-param-strings
+  (is
+    (=
+      (create-param-strings "name" "mike" "age" 10)
+      '("name=mike", "age=10")))
   )
 
 (deftest test-complete-request-params
-  (is (= (complete-request-params "my-method" {"foo" "bar"}) '("api_key=1c46840d769cbf7e2281680ea58a45ed" "foo=bar" "format=rest" "method=my-method" "user_id=67155975@N00")))
+  (is
+    (=
+      (complete-request-params "my-method" "foo" "bar")
+      '("api_key=1c46840d769cbf7e2281680ea58a45ed" "foo=bar" "format=rest" "method=my-method" "user_id=67155975@N00")))
   )
 
 (deftest test-create-url
-  (is (= (create-url "my-method" {"foo" "bar" "bar" "foo"})
-        "http://api.flickr.com/services/rest/?api_key=1c46840d769cbf7e2281680ea58a45ed&bar=foo&foo=bar&format=rest&method=my-method&user_id=67155975@N00"))
+  (is
+    (=
+      (create-url "my-method" "foo" "bar" "bar" "foo")
+      "http://api.flickr.com/services/rest/?api_key=1c46840d769cbf7e2281680ea58a45ed&bar=foo&foo=bar&format=rest&method=my-method&user_id=67155975@N00"))
   )
 
-(deftest test-call
-  (is (= (get-public-photos 10) "foobar"))
+(deftest test-match-tag
+  (is
+    (nil?
+      (match-tag {:tag "foo" :attrs "test"} "bar")))
+  (is
+    (=
+      (match-tag {:tag "foo" :attrs "test"} "foo")
+      "test"))
+  )
+
+(deftest test-construct-photo-source-url
+  (is
+    (=
+      (construct-photo-source-url {:farm "f1" :server "s1" :id "i1" :secret "s2"} "s")
+      "http://farmf1.staticflickr.com/s1/i1_s2_s.jpg"))
+  )
+
+(deftest test-get-page-count
+  (is
+    (=
+      (get-page-count 500 50)
+      10.0))
   )

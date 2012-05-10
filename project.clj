21d0b9ed21de620814859e76f8ff01b2b115382b
Mike Hurd <leahcim77@gmail.com>
2012-05-10 17:10:51 +0100

    NOTICKET: added marginalia documentation

--- project.clj
+++ project.clj
@@ -1,7 +1,10 @@
 (defproject org.hurdy.flickr "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Clojure Flickr"
   :dependencies [
                   [org.clojure/clojure "1.4.0"]
                   [clj-http "0.1.1"]
                 ]
+  :plugins  [
+              [lein-marginalia "0.7.0"]
+            ]
 )
--- src/org/hurdy/flickr/core.clj
+++ src/org/hurdy/flickr/core.clj
@@ -10,82 +10,96 @@
 (use '[clojure.xml :only (parse)])
 (use '[clojure.java.io :only [as-file]])
 
-(def flickr-key "1c46840d769cbf7e2281680ea58a45ed")
+(def
+  ^{:doc "Your flickr API key passed with calls"}
+  flickr-key "1c46840d769cbf7e2281680ea58a45ed")
 
-(def flickr-consumer-secret "2cd273980ccde5db")
+(def
+  ^{:doc "Your flickr API secret used to sign calls"}
+  flickr-consumer-secret "2cd273980ccde5db")
 
-(def flickr-user-id "67155975@N00")
+(def
+  ^{:doc "The Flickr ID to use"}
+  flickr-user-id "67155975@N00")
 
-(def flickr-rest-api "http://api.flickr.com/services/rest/?")
+(def
+  ^{:doc "Base Flickr REST API URL"}
+  flickr-rest-api "http://api.flickr.com/services/rest/?")
 
-(defn create-param-strings [& params]
-  ; accept a variable number of parameters in assumed key/value/key/value order
-  ; turn the list of parameters into a map, deconstruct each key/value pair one at a time
-  ; in a for loop then turn the pair into an '=' separated String and collect into
-  ; a list (via the for) and return the list
-  (for [[k v] (apply hash-map params)]
-    (str k "=" v))
+(defn create-param-strings
+  "Accept a variable number of parameters in assumed key/value/key/value order
+  turn the list of parameters into a map, deconstruct each key/value pair one at a time
+  in a for loop then turn the pair into an '=' separated String and collect into
+  a list (via the for) and return the list"
+  [& params]
+    (for [[k v] (apply hash-map params)]
+      (str k "=" v))
   )
 
-(defn complete-request-params [method-name & request-params]
-  ; accept a method name and a variable number of parameters.
-  ; create the basic set of parameters then create the extra
-  ; parameters, turn the results into one list and return the
-  ; list sorted (by key effectively)
-  (sort
-    (concat
-      (create-param-strings "method" method-name,
-        "format" "rest",
-        "api_key" flickr-key,
-        "user_id" flickr-user-id)
-      (apply create-param-strings request-params)))
+(defn complete-request-params
+  "Accept a method name and a variable number of parameters.
+  create the basic set of parameters then create the extra
+  parameters, turn the results into one list and return the
+  list sorted (by key effectively)"
+  [method-name & request-params]
+    (sort
+      (concat
+        (create-param-strings "method" method-name,
+          "format" "rest",
+          "api_key" flickr-key,
+          "user_id" flickr-user-id)
+        (apply create-param-strings request-params)))
   )
 
-(defn create-url [method & request-params]
-  ; create the url to call by joining the base flickr rest
-  ; url with the processed parameters
-  (str
-    flickr-rest-api
-    (join "&" (apply complete-request-params method request-params)))
-  )
-
-(defn flickr-people-get-public-photos [page number-of-photos-per-page]
-  ; use the xml parse function to call the flickr.people.getPublicPhotos method
-  ; getting the specified page given the number of photos per page required
-  (parse
-    (create-url "flickr.people.getPublicPhotos" "per_page" number-of-photos-per-page "page" page))
-  )
-
-(defn construct-photo-source-url [photo-attrs size]
-  ; creates the actual photo URL given the photo element attributes as
-  ; retrieved from the Flickr API.
-  ; use let to extract the named keys from the supplied photo attributes map
-  ; and use them as local variables
-  (let [{:keys [farm server id secret]} photo-attrs]
+(defn create-url
+  "Create the url to call by joining the base flickr rest
+  url with the processed parameters"
+  [method & request-params]
     (str
-      "http://farm"
-      farm
-      ".staticflickr.com/"
-      server
-      "/"
-      id
-      "_"
-      secret
-      "_"
-      size
-      ".jpg")
-    )
+      flickr-rest-api
+      (join "&" (apply complete-request-params method request-params)))
+  )
+
+(defn flickr-people-get-public-photos
+  "Use the xml parse function to call the flickr.people.getPublicPhotos method
+  getting the specified page given the number of photos per page required"
+  [page number-of-photos-per-page]
+    (parse
+      (create-url "flickr.people.getPublicPhotos" "per_page" number-of-photos-per-page "page" page))
+  )
+
+(defn construct-photo-source-url
+  "Creates the actual photo URL given the photo element attributes as
+  retrieved from the Flickr API.
+  use let to extract the named keys from the supplied photo attributes map
+  and use them as local variables"
+  [photo-attrs size]
+    (let [{:keys [farm server id secret]} photo-attrs]
+      (str
+        "http://farm"
+        farm
+        ".staticflickr.com/"
+        server
+        "/"
+        id
+        "_"
+        secret
+        "_"
+        size
+        ".jpg")
+      )
   )
 
 (defn get-page-count [total-pages photos-per-page]
   (Math/ceil (/ total-pages photos-per-page))
   )
 
-(defn get-attributes-of-matching-tag [element tag-to-match]
-  ; if the :tag of the specified element matches
-  ; the expectedTag the return the :attrs of the element
-  ; otherwise return nil
-  (if (= tag-to-match (:tag element)) (:attrs element))
+(defn get-attributes-of-matching-tag
+  "If the :tag of the specified element matches
+  the expectedTag the return the :attrs of the element
+  otherwise return nil"
+  [element tag-to-match]
+    (if (= tag-to-match (:tag element)) (:attrs element))
   )
 
 (defn get-attributes-of-matching-tags [flickr-xml-seq tag-to-match]
@@ -145,12 +159,13 @@
     )
 )
 
-(defn strip-non-colors [rgb-frequencies]
-  ; remove rgb values where R & G & B < 30 or r & G & B > 225
-  ; mmh, how to do this on the frequency map that looks
-  ; like {:r 187, :g 194, :b 213} 1, {:r 213, :g 175, :b 6} 1
-  ; ... till' I work this out just return the map back again.
-  rgb-frequencies
+(defn strip-non-colors
+  "Remove rgb values where R & G & B < 30 or r & G & B > 225
+  mmh, how to do this on the frequency map that looks
+  like {:r 187, :g 194, :b 213} 1, {:r 213, :g 175, :b 6} 1
+  ... till' I work this out just return the map back again."
+  [rgb-frequencies]
+    rgb-frequencies
   )
 
 (defn get-dominant-rgb-value [url]
@@ -175,8 +190,9 @@
     )
   )
 
-(defn get-all-public-photo-source-urls [size]
-  ; Get a sample page to determine the number of pages given a max size of 500 photos per-page
+(defn get-all-public-photo-source-urls
+  "Get a sample page to determine the number of pages given a max size of 500 photos per-page."
+  [size]
   (let
     [max-photos-per-page 500
     flickr-xml-seq (xml-seq (flickr-people-get-public-photos 1 1))
